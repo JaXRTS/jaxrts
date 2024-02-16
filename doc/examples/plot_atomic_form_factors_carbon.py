34c4a333bdfa30aac0c8ef5a2d54bfd23ad5314d
lue <julian.luetgert@uni-rostock.de>
2024-02-16 10:28:01 +0100

    Create __init__.py by hand

--- doc/examples/plot_atomic_form_factors_carbon.py
+++ doc/examples/plot_atomic_form_factors_carbon.py
@@ -10,6 +10,7 @@ presented in :cite:`Pauling.1932`.
     and so on, directly, you could also use
     :py:func:`jaxrts.form_factors.pauling_atomic_ff`, instead.
 """
+
 import numpy as np
 import matplotlib.pyplot as plt
 import scienceplots
@@ -23,38 +24,40 @@ k = np.linspace(0, 10, 100) / ureg.angstrom
 
 CZ1s, CZ2s, CZ2p = ff.pauling_effective_charge(6)[:3]
 
-fC10 = ff.pauling_f10(k, CZ1s).m_as(ureg.dimensionless)
-fC20 = ff.pauling_f20(k, CZ2s).m_as(ureg.dimensionless)
-fC21 = ff.pauling_f21(k, CZ2s).m_as(ureg.dimensionless)
+fC10 = ff.pauling_f10(k, CZ1s)
+fC20 = ff.pauling_f20(k, CZ2s)
+fC21 = ff.pauling_f21(k, CZ2s)
 
 plt.style.use("science")
-plt.plot(k.m_as(1/ureg.angstrom), fC10 + fC20 + fC21, label="Full form factor")
+plt.plot(
+    k.m_as(1 / ureg.angstrom),
+    (fC10 + fC20 + fC21).m_as(ureg.dimensionless),
+    label="Full form factor",
+)
 
 plt.plot(
-    k.m_as(1/ureg.angstrom),
-    fC10,
+    k.m_as(1 / ureg.angstrom),
+    fC10.m_as(ureg.dimensionless),
     label=r"$1s$ electrons",
     color="grey",
     linestyle=((0, (1, 1))),
 )
 plt.plot(
-    k.m_as(1/ureg.angstrom),
-    fC20,
+    k.m_as(1 / ureg.angstrom),
+    fC20.m_as(ureg.dimensionless),
     label=r"$2s$ electrons",
     color="grey",
     linestyle=((0, (5, 1))),
 )
 plt.plot(
-    k.m_as(1/ureg.angstrom),
-    fC21,
+    k.m_as(1 / ureg.angstrom),
+    fC21.m_as(ureg.dimensionless),
     label=r"$2p$ electrons",
     color="grey",
     linestyle=(0, (3, 1, 1, 1, 1, 1)),
 )
 
-plt.title(
-    "Atomic form factors for Carbon\n(Pauling and Sherman)"
-)
+plt.title("Atomic form factors for Carbon\n(Pauling and Sherman)")
 
 plt.xlabel(r"$q$ [1/$\AA$]")
 plt.ylabel(r"$f_i$")
--- src/jaxrts/__init__.py
+++ src/jaxrts/__init__.py
@@ -1,12 +1,5 @@
-# To generate the __init__.py automatically, use Erotemic's great
-# mkinit: https://github.com/Erotemic/mkinit/
-#
-# Run the command 'mkinit --relative src/athens -w'
-
 __version__ = "0.1.0"
-
-
-# <AUTOGEN_INIT>
+ 
 from . import bound_free
 from . import core_electron
 from . import form_factors
@@ -19,18 +12,9 @@ from . import plasmastate
 from . import plotting
 from . import units
 
-from .form_factors import (logger, pauling_atomic_ff, pauling_f10, pauling_f20,
-                           pauling_f21, pauling_f30, pauling_f31, pauling_f32,
-                           pauling_f40, pauling_f41, pauling_f42, pauling_f43,
-                           pauling_size_screening_constants, pauling_xf,)
 from .plasmastate import (PlasmaState,)
 from .units import (ureg,)
 
 __all__ = ['PlasmaState', 'bound_free', 'core_electron', 'form_factors',
            'free_bound', 'helpers', 'instrument_function', 'ion_feature',
-           'logger', 'pauling_atomic_ff', 'pauling_f10', 'pauling_f20',
-           'pauling_f21', 'pauling_f30', 'pauling_f31', 'pauling_f32',
-           'pauling_f40', 'pauling_f41', 'pauling_f42', 'pauling_f43',
-           'pauling_size_screening_constants', 'pauling_xf', 'plasmastate',
-           'plotting', 'units', 'ureg']
-# </AUTOGEN_INIT>
+           'plasmastate', 'plotting', 'units', 'ureg']
