d997e72cdb25c9128f04dc82e09bc2a6adf755f2
lue <julian.luetgert@uni-rostock.de>
2024-07-04 10:18:43 +0200

    jpu fixes in compton shift

--- src/jaxrts/plasma_physics.py
+++ src/jaxrts/plasma_physics.py
@@ -317,7 +317,7 @@ def compton_energy(probe_energy, scattering_angle):
                 1
                 + probe_energy
                 / (1 * ureg.electron_mass * ureg.speed_of_light**2)
-                * (1 - jnp.cos(jnp.deg2rad(scattering_angle)))
+                * (1 - jnpu.cos(scattering_angle))
             )
         )
     ).to(ureg.electron_volt)
