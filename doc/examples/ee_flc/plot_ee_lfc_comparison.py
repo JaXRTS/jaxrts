ee98efe45e0d0fb11f4120909ce9d9e4bc1cdb34
lue <julian.luetgert@uni-rostock.de>
2024-07-09 15:43:26 +0200

    ee_lfc example

---
+++ doc/examples/ee_flc/plot_ee_lfc_comparison.py
@@ -0,0 +1,38 @@
+import pathlib
+
+import numpy as onp
+import jax.numpy as jnp
+import jpu.numpy as jnpu
+import jax
+import pytest
+import matplotlib.pyplot as plt
+
+import jaxrts
+
+ureg = jaxrts.ureg
+
+
+def test_staticInterp_Gregori2007_reproduction():
+    for T in [4, 20]:
+        T = T * ureg.electron_volt / ureg.boltzmann_constant
+        n_e = 2.5e23 / ureg.centimeter**3
+
+        kf = (3 * jnp.pi**2 * n_e) ** (1 / 3)
+        k = jnpu.linspace(0 * kf, 10 * kf, 5000)
+
+        G_calc = (
+            jaxrts.ee_localfieldcorrections.eelfc_interpolationgregori2007(
+                k, T, n_e
+            )
+        )
+        G_farid = (
+            jaxrts.ee_localfieldcorrections.eelfc_interpolationgregori_farid(
+                k, T, n_e
+            )
+        )
+        plt.plot(k/kf, G_calc.m_as(ureg.dimensionless))
+        plt.plot(k/kf, G_farid)
+
+    plt.show()
+
+test_staticInterp_Gregori2007_reproduction()
