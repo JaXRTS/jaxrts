5386b612e86fc6c09bdde07e0c7b7a12b0b04b98
lue <julian.luetgert@uni-rostock.de>
2025-02-21 09:27:36 +0100

    Add comparison to MD simulation to example

--- doc/examples/ionic/plot_LatticeDebyeModel.py
+++ doc/examples/ionic/plot_LatticeDebyeModel.py
@@ -4,17 +4,31 @@ LatticeDebyeModel for approximating diffuse scattering in crystals
 
 This example showcases the increased diffuse scattering in heated crystals by
 the Debye Waller Effect.
+
+For :math:`S_\\text{Plasma}` the model assumes a fixed value of unity, for all
+:math:`k`.
+A comparison with MD-simulations helps to asses the quality of this very simple
+model. The simulation was performed with 8000 atoms using the software package
+LAMMPS :cite:`Thompson.2022`. For the interatomic potential, the work by and
+Purja Pun and Mishin :cite:`Pun.2017` was used, which improves on the Tersoff
+potential. Static ion-ion structure factors where calculated by averaging over
+scatting vectors realizing the same absolute :math:`k`, and over 10000 time
+steps.
 """
+
+from pathlib import Path
 
 import jax.numpy as jnp
 import matplotlib.pyplot as plt
+import matplotlib.lines as mlines
 import scienceplots
+import numpy as onp
 
 import jaxrts
 
 ureg = jaxrts.ureg
 plt.style.use("science")
-
+fig, ax = plt.subplots()
 
 theta_Debye = ureg("692K")
 
@@ -23,6 +37,11 @@ rho0 = ureg("2.329085g/cc")
 rhos = rho0
 a0 = ureg("543.0986 pm")
 a = (rho0 / rhos) ** (1 / 3) * a0
+
+try:
+    current_folder = Path(__file__).parent
+except NameError:
+    current_folder = Path.cwd()
 
 
 def peak_function(k):
@@ -94,25 +113,46 @@ _setup = jaxrts.Setup(ureg("90°"), None, None, lambda x: x)
 state = jaxrts.PlasmaState([jaxrts.Element("Si")], [0], [rhos], ureg("300K"))
 state["Debye temperature"] = jaxrts.models.ConstantDebyeTemp(theta_Debye)
 
-for T in jnp.array([300, 800, 1300]) * ureg.kelvin:
+for idx, T in enumerate(jnp.array([300, 800, 1300]) * ureg.kelvin):
     Sii = []
     state.T_e = T
     state.T_i = jnp.array([T.m_as(ureg.kelvin)]) * ureg.kelvin
     for k in probe_k:
         setup = jaxrts.setup.get_probe_setup(k, _setup)
         Sii_out = DebyeSolid.S_ii(state, setup)
-        print(Sii_out)
         Sii.append(Sii_out[0, 0])
 
     Sii = jnp.array(Sii)
-    plt.plot(
-        probe_k.m_as(1 / ureg.angstrom), Sii, label=f"{T.m_as(ureg.kelvin)}K"
+    ax.plot(
+        probe_k.m_as(1 / ureg.angstrom),
+        Sii,
+        label=f"{T.m_as(ureg.kelvin)}K",
+        color=f"C{idx}",
     )
+    try:
+        kMD, SiiMD = onp.genfromtxt(
+            current_folder / f"MD-sims/Si{T.m_as(ureg.kelvin)}K.dat",
+            unpack=True,
+        )
+        MD_sort = onp.argsort(kMD)
+        ax.plot(kMD[MD_sort], SiiMD[MD_sort], color=f"C{idx}", alpha=0.4)
+    except FileNotFoundError:
+        pass
 
-plt.yscale("log")
+ax.set_yscale("log")
 
-plt.ylabel("$S_{ii}(k)$")
-plt.xlabel("$k$ [1/\\AA]")
+ax.set_ylabel("$S_{ii}(k)$")
+ax.set_xlabel("$k$ [1/\\AA]")
+ax.set_xlim([1, 8])
 
-plt.legend()
+first_legend = ax.legend()
+# Add a second legend
+line1 = mlines.Line2D([0], [0], color="C0", label="DebyeWallerSolid")
+line2 = mlines.Line2D([0], [0], color="C0", alpha=0.4, label="MD-Simulation")
+second_legend = plt.legend(
+    handles=[line1, line2], loc="center", ncol=2, bbox_to_anchor=(0.5, 1.1)
+)
+# fig.add_artist(second_legend)
+fig.add_artist(first_legend)
+
 plt.show()
