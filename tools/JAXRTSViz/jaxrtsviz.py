743cc0bf149e6cfb286e49162c3641e08ccc98e0
lue <julian.luetgert@uni-rostock.de>
2025-03-05 15:25:01 +0100

    Cache compiled functions on linux

--- tools/JAXRTSViz/jaxrtsviz.py
+++ tools/JAXRTSViz/jaxrtsviz.py
@@ -8,6 +8,12 @@ import sys
 from functools import partial
 
 import jax
+
+if sys.platform == "linux":
+    jax.config.update("jax_compilation_cache_dir", "/tmp/jax_cache")
+    jax.config.update("jax_persistent_cache_min_entry_size_bytes", -1)
+    jax.config.update("jax_persistent_cache_min_compile_time_secs", 0.2)
+
 import jax.numpy as jnp
 import matplotlib.colors as mcolors
 import matplotlib.pyplot as plt
