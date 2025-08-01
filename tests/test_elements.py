36644ee131e27da7f9d4b84353349cd620a97504
Chongbing Qu <quchongbing@gmail.com>
2025-08-01 13:17:21 +0200

    Test proximity of cold vs non-ionized edges

--- tests/test_elements.py
+++ tests/test_elements.py
@@ -1,6 +1,7 @@
 import numpy as onp
+import jpu.numpy as jnpu
 
-from jaxrts import elements
+from jaxrts import elements, ureg
 
 
 def test_element_creatation() -> None:
@@ -23,3 +24,39 @@ def test_electron_distribution() -> None:
         assert electron_occupation[7] <= 6
         assert electron_occupation[8] <= 10
         assert electron_occupation[9] <= 14
+
+
+def test_cold_ionization_energies_match_zero_ionization() -> None:
+    """
+    Test that the cold ionization energies match the zero
+    ionization energies.
+    """
+    acceptable_deviation = ureg("10eV")
+    for z in range(35):
+        element = elements.Element(z + 1)
+        if element == elements.Element("N") or element == elements.Element(
+            "O"
+        ):
+            # The L edge caused by the s orbital is quite off for
+            # nitrogen and oxygen.
+            assert (
+                element.cold_binding_energies[0]
+                - element.get_binding_energies(0)[0]
+            ) < acceptable_deviation
+            assert (
+                element.cold_binding_energies[1]
+                - element.get_binding_energies(0)[1]
+            ) < 2 * acceptable_deviation
+
+            assert (
+                element.cold_binding_energies[2]
+                - element.get_binding_energies(0)[2]
+            ) < acceptable_deviation
+        else:
+            assert (
+                jnpu.max(
+                    element.cold_binding_energies
+                    - element.get_binding_energies(0)
+                )
+                < acceptable_deviation
+            )
