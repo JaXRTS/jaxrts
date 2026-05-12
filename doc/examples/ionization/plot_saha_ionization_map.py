48a20fccb7995db38ad11ef0c55ea89386261fd1
lue <julian.luetgert@uni-rostock.de>
2026-05-12 18:42:23 +0200

    docs: fix typo in docstring

--- doc/examples/ionization/plot_saha_ionization_map.py
+++ doc/examples/ionization/plot_saha_ionization_map.py
@@ -4,7 +4,7 @@ Saha Ionization Map
 
 This example plots the ionization state of a CH plasma as a two-dimensional
 quantity over temperature and density. Different selections for the IPD model
-(here, :py:class:`jaxrts.model.StewartPyattIPD`) can be used for the
+(here, :py:class:`jaxrts.models.StewartPyattIPD`) can be used for the
 calculation, moving the ionization considerably.
 """
 
